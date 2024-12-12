9c2f61559aa28b9edd22fdaa833e8c79a6899fc2
Puru Parmar <puruparmar@Purus-MacBook-Air.local>
2024-12-12 17:02:23 +0530

    episode 02

--- App.js
+++ App.js
@@ -1,3 +1,5 @@
+        import React from "react";
+        import ReactDOM from "react-dom/client";
         const reactHeading = React.createElement("h1",{id : "rootElement"},
             React.createElement("h2",{id:"rootElement2"},
               [[React.createElement("h3",{id:"root3"},"Im h3"),"I'm inside h2"],[React.createElement("h3",{id:"root3"},"Im h3"),"I'm inside h2"]]));
